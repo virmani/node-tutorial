5b0392a5c3769a60f1d34bd93fc0628c1ad044c1
Ashish Virmani <ashishvirmani@hotmail.com>
2012-06-10 12:43:16 -0700

    Converting the turorial into a service for uploading images to S3.
    Added Knox (with the fix for node running on Windows)
    Added the call to S3 to store the image up there.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,65 +1,93 @@
 var querystring = require('querystring'),
-		fs = require("fs"),
-		formidable = require("formidable");
+        fs = require("fs"),
+        formidable = require("formidable");
+
+var S3_KEY = '<your key here>';
+var S3_SECRET = '<your secret here>';
+var S3_BUCKET = '<your bucket here>';
+
+var knox = require('knox').createClient({
+    key: S3_KEY,
+    secret: S3_SECRET,
+    bucket: S3_BUCKET
+});
 
 function start(response) {
-  console.log("Request handler 'start' was called.");
+    console.log("Request handler 'start' was called.");
 
-	var body = '<html>'+
-		'<head>'+
-		'<meta http-equiv="Content-Type" '+
-		'content="text/html; charset=UTF-8" />'+
-		'</head>'+
-		'<body>'+
-		'<form action="/upload" enctype="multipart/form-data" '+
-		'method="post">'+
-		'<input type="file" name="upload">'+
-		'<input type="submit" value="Upload file" />'+
-		'</form>'+
-		'</body>'+
-		'</html>';
-	
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write(body);
-	response.end();
+    var body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" ' +
+        'content="text/html; charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" enctype="multipart/form-data" ' +
+        'method="post">' +
+        '<input type="file" name="upload">' +
+        '<input type="submit" value="Upload file" />' +
+        '</form>' +
+        '</body>' +
+        '</html>';
+
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(body);
+    response.end();
 
 }
 
 function upload(response, request) {
-	console.log("Request handler 'upload' was called.");
-	var form = new formidable.IncomingForm();
-	form.parse(request, function(error, fields, files) {
-		fs.rename(files.upload.path, "/tmp/test.png", function(err) {
-			if(err)
-			{
-				console.log("Failed due to error: " + err);
-				fs.unlink("/tmp/test.png");
-				fs.rename(files.upload.path, "/tmp/test.png");
-			}
-			console.log("Image wrritten to disk");
-		});
-		
-		response.writeHead(200, {"Content-Type": "text/html"});
-		response.write("Image Received <br/><br/>");
-		response.write("<img src='/show' />");
-		response.end();
-	});
-	
+    console.log("Request handler 'upload' was called.");
+    var form = new formidable.IncomingForm();
+
+    form.parse(request, function (error, fields, files) {
+        if (error != null) {
+            console.log('Failed to read the form. Error: ' + error);
+            response.writeHead(500, { "Content-Type": "text/html" });
+            response.write("Failed to upload<br/>");
+            response.end();
+        }
+
+        try {
+            knox.putFile(files.file.path, 'tests/' + files.file.name, { 'Content-Type': 'image/jpeg' },
+                function (err, result) {
+                    if (200 == result.statusCode) {
+                        console.log('Uploaded to Amazon S3');
+                        response.writeHead(200, { "Content-Type": "text/html" });
+                        response.write("Image Received <br/><br/>");
+                        response.write("<img src='https://s3.amazonaws.com/appmania-food-images/sampleFile.png' />");
+                        response.end();
+                    }
+                    else {
+                        console.log('Failed to upload file to Amazon S3');
+                        response.writeHead(500, { "Content-Type": "text/html" });
+                        response.write("Failed to upload<br/>");
+                        response.end();
+                    }
+                });
+        } catch (error) {
+            console.log('Exception occured while using Knox. Error: ' + error);
+            response.writeHead(500, { "Content-Type": "text/html" });
+            response.write("Failed to upload<br/>");
+            response.end();
+        }
+    });
+
 }
 
 function show(response, postData) {
-	console.log("Request handler 'show' was called.");
-	fs.readFile("/tmp/test.png", "binary", function(error, file) {
-		if(error) {
-			response.writeHead(500, {"Content-Type": "text/plain"});
-			response.write(error + "\n");
-			response.end();
-		} else {
-			response.writeHead(200, {"Content-Type": "image/png"});
-			response.write(file, "binary");
-			response.end();
-		}
-	});
+    console.log("Request handler 'show' was called.");
+
+    fs.readFile("/tmp/test.png", "binary", function (error, file) {
+        if (error) {
+            response.writeHead(500, { "Content-Type": "text/plain" });
+            response.write(error + "\n");
+            response.end();
+        } else {
+            response.writeHead(200, { "Content-Type": "image/png" });
+            response.write(file, "binary");
+            response.end();
+        }
+    });
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ function start(route, handle)
 	http.createServer(function(request, response) {
 		var pathName = url.parse(request.url).pathname;
 		route(pathName, handle, response, request);
-	}).listen(8080);
+	}).listen(8088);
 }
 
 exports.start = start;
